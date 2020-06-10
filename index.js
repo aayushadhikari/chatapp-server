a38a669a42b622a32c479aaabf5e684b1aefd74e
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-06-10 18:11:35 -0400

    message

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const socketio = require("socket.io");
 const http = require("http");
 const PORT = process.env.PORT || 3005;
 const router = require("./router");
+const { addUser, removeUser, getUser, getUsersInRoom } = require("./users");
 
 const app = express();
 app.use(router);
@@ -12,7 +13,26 @@ io.on("connection", (socket) => {
   console.log("New connection received");
 
   socket.on("join", ({ name, room }, callback) => {
-    console.log(name);
+    const { error, user } = addUser({ id: socket.id, name, room });
+
+    if (error) return callback(error);
+
+    socket.emit("message", {
+      user: "admin",
+      text: `${user.name}, welcome to the room ${user.room}`,
+    });
+    socket.broadcast
+      .to(user.room)
+      .emit("message", { user: "admin", text: `${user.name}, has joined!` });
+    socket.join(users.room);
+    callback();
+  });
+
+  socket.on("sendMessage", (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit("message", { user: user.name, text: message });
+    callback();
   });
 
   socket.on("disconnect", () => {
