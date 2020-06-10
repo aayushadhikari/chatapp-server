6705f486581bbc79440dd528bac220d34032ca8c
Aayush Adhikari <aayushadhikari11@gmail.com>
2020-06-10 17:22:02 -0400

    connections

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const PORT = process.env.PORT || 3005;
+const router = require("./router");
+
+const app = express();
+app.use(router);
+const server = http.createServer(app);
+const io = socketio(server);
+io.on("connection", (socket) => {
+  console.log("New connection received");
+
+  socket.on("disconnect", () => {
+    console.log("User left");
+  });
+});
+server.listen(PORT, () => {
+  console.log(`Server is listening on ${PORT}`);
+});
